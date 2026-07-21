7da4e5fabb066bfa2b9cce66e5be52e5a820ec0f
yunyu67 <5763394@qq.com>
2026-07-22 03:52:26 +0800

    Add original_output_sample_rate/SetOutputSampleRate to AudioCodec

--- main/audio/audio_codec.h
+++ main/audio/audio_codec.h
@@ -42,6 +42,13 @@ public:
     inline bool input_reference() const { return input_reference_; }
     inline int input_sample_rate() const { return input_sample_rate_; }
     inline int output_sample_rate() const { return output_sample_rate_; }
+    inline int original_output_sample_rate() const { return original_output_sample_rate_ > 0 ? original_output_sample_rate_ : output_sample_rate_; }
+    virtual bool SetOutputSampleRate(int rate) {
+        if (original_output_sample_rate_ == 0) original_output_sample_rate_ = output_sample_rate_;
+        if (rate == -1) output_sample_rate_ = original_output_sample_rate_;
+        else output_sample_rate_ = rate;
+        return true;
+    }
     inline int input_channels() const { return input_channels_; }
     inline int output_channels() const { return output_channels_; }
     inline int output_volume() const { return output_volume_; }
@@ -59,6 +66,7 @@ protected:
     bool output_enabled_ = false;
     int input_sample_rate_ = 0;
     int output_sample_rate_ = 0;
+    int original_output_sample_rate_ = 0;
     int input_channels_ = 1;
     int output_channels_ = 1;
     int output_volume_ = 70;
